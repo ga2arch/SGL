d6ae51242f0d6466ca963624a4bc581ef4a3b65d
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-10-25 18:11:28 +0200

    firt commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SGL
+//
+//  Created by Gabriele on 25/10/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
