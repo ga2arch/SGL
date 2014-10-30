b45fdc0d93978a73349d731212367849d94a4d0b
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-10-30 16:12:44 +0100

    added DoubleBufferedAllocator

---
+++ DoubleBufferedAllocator.cpp
@@ -0,0 +1,13 @@
+//
+//  DoubleBufferedAllocator.cpp
+//  SGL
+//
+//  Created by Gabriele Carrettoni on 30/10/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#include "DoubleBufferedAllocator.h"
+
+void DoubleBufferedAllocator::swap_buffers() {
+    
+}
---
+++ DoubleBufferedAllocator.h
@@ -0,0 +1,30 @@
+//
+//  DoubleBufferedAllocator.h
+//  SGL
+//
+//  Created by Gabriele Carrettoni on 30/10/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#ifndef __SGL__DoubleBufferedAllocator__
+#define __SGL__DoubleBufferedAllocator__
+
+#include <stdio.h>
+#include <ostream>
+#include "StackAllocator.h"
+
+class DoubleBufferedAllocator {
+    
+public:
+    
+    void swap_buffers();
+    void clear_current();
+    void* alloc(uint32_t bytes);
+    
+private:
+    uint32_t cursor_stack;
+    StackAllocator stacks[2];
+    
+};
+
+#endif /* defined(__SGL__DoubleBufferedAllocator__) */
