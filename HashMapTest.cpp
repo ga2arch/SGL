d52a200953beeeaf40bc78de99f789f0c3d41dd2
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-02 22:46:34 +0100

    configured gtest

---
+++ HashMapTest.cpp
@@ -0,0 +1,14 @@
+//
+//  HashMapTest.cpp
+//  SGL
+//
+//  Created by Gabriele Carrettoni on 02/11/14.
+//  Copyright (c) 2014 Ga2 & co. All rights reserved.
+//
+
+#include "HashMapTest.h"
+#include <gtest/gtest.h>
+
+TEST(wtf, lol) {
+    ASSERT_EQ(1, 1);
+}
