9ca78b5588067764733de0f739f8c7eb3c4cae19
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-03 12:59:12 +0100

    added more hashmap tests

--- tests/HashMapTest.cpp
+++ tests/HashMapTest.cpp
@@ -12,22 +12,43 @@
 
 TEST(HashMapTest, ThrowsOnNotFoundKey) {
     HashMap<int, std::string, 10> m;
+    
     ASSERT_THROW(m.get(10), std::invalid_argument);
 }
 
-TEST(HashMapTest, PutGetKey) {
+TEST(HashMapTest, PutGetKeyByValue) {
     HashMap<int, std::string, 10> m;
     std::string v("ciao");
     m.put(10, v);
-    auto v1 = m.get(10);
-    ASSERT_EQ(v1, v);
+    
+    ASSERT_EQ(m.get(10), v);
 }
 
-TEST(HashMapTest, RemoveKey) {
+TEST(HashMapTest, PutGetKeyByPointer) {
+    HashMap<int, std::string*, 10> m;
+    std::string* v = new std::string("ciao");
+    m.put(10, v);
+
+    ASSERT_EQ(*m.get(10), *v);
+    delete v;
+}
+
+TEST(HashMapTest, RemoveKeyByValue) {
     HashMap<int, std::string, 10> m;
     std::string v("ciao");
     m.put(10, v);
     m.remove(10);
+    
+    ASSERT_THROW(m.get(10), std::invalid_argument);
+}
+
+TEST(HashMapTest, RemoveKeyByPointer) {
+    HashMap<int, std::string*, 10> m;
+    std::string* v = new std::string("ciao");
+    
+    m.put(10, v);
+    m.remove(10);
+    
     ASSERT_THROW(m.get(10), std::invalid_argument);
 }
 
