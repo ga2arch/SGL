e26150fb8b6adc929007b727e6eab047cb90780f
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-14 20:57:23 +0100

    cleaned

--- SGL/main.cpp
+++ SGL/main.cpp
@@ -70,91 +70,11 @@ public:
 
 };
 
-struct Zap2 {
-    int v;
-    
-    Zap2() {};
-    Zap2(int v_): v(v_) {};
-};
-
-template<typename F, typename ...Args>
-typename std::chrono::nanoseconds::rep execution(F func, Args&&... args) {
-    auto start = std::chrono::high_resolution_clock::now();
-    
-    // Now call the function with all the parameters you need.
-    func(std::forward<Args>(args)...);
-    
-    auto duration =
-    std::chrono::
-    duration_cast<std::chrono::nanoseconds>(std::chrono::
-                         high_resolution_clock::now() - start);
-    
-    return duration.count();
-}
-
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-//    FixedHashMap<int, unique_ptr<Zap>, 10> m;
-//    
-//    auto z = sgl::make_unique<Zap>(10);
-//    
-//    m.put(10, std::move(z));
-//    auto z1 = m.pop(10);
-//    
-//    cout << z1->v << endl;
-//    
-//    FixedArray<Zap, 10> a;
-//    
-//    Zap z2(10);
-//    a.insert<0>(Zap(10));
-//    auto& b = a.get<0>();
-//    
-//    b.v = 2;
-//    
-//    cout << a.get<0>().v << endl;
-//    
-//    sgl::memory::PoolAllocator pool(10, sizeof(Zap));
-//    Queue<Zap> q(pool);
-//    
-//    q.emplace(10);
-//    
-//    auto&& z3 = q.dequeue();
-//    
-//    cout << z3.v << endl;
-    
-//     cout << execution(([]() {
-//              volatile int forceNoOptimizeDummy;
-//        const size_t MAX = 300 * 1000;
-//
-//    ProducerConsumerQueue<int> q(MAX);
-//    
-//        int element = -1;
-//        int* n = new int(10);
-//
-//    std::thread producer([&]() {
-//        size_t num = 0;
-//        for (size_t i = 0; i != MAX; ++i) {
-//            q.enqueue(10);
-//            ++num;
-//        }
-//    });
-//    
-//        std::thread consumer([&]() {
-//            for (size_t i = 0; i != MAX; ++i) {
-//                q.try_dequeue(element);
-//            }
-//        });
-//        
-//    producer.join();
-//    consumer.join();
-//    forceNoOptimizeDummy = (bool)(q.try_dequeue(element));
-//
-//    }));
-
     ProducerConsumerQueue<Zap> q(10);
     
-    q.enqueue(10);
     q.enqueue(20);
     Zap z;
     
