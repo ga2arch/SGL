0ead0ca3f328ac51f79a96a642b672c23d5b5052
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-19 16:36:30 +0100

    added test scheme, add test for vector creation

--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -9,8 +9,6 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-#include "FixedHashMapTest.h"
-
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
